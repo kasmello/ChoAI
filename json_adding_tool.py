7620d4ac57da3305ea587921d3bbd51e205397bd
kasmello <karmel.gandahusada@gmail.com>
2024-09-16 17:16:43 +0800

    more data :)

--- json_adding_tool.py
+++ json_adding_tool.py
@@ -1,7 +1,69 @@
+import os
+import glob
 import json
-import sys
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import matplotlib.font_manager as fm
+import matplotlib.image as mpimg
 
-with open('ProcessedData/data.json', 'w') as json_file:
-    data = json.load(json_file, indent=4) 
+# Specify the directory
+folder_path = "Training"
+df = pd.read_csv('processeddata/joined_df.csv')
+df
 
-print(data)
+manjari_bold_path = 'font/Manjari-Bold.ttf'
+manjari_regular_path = 'font/Manjari-Regular.ttf'
+manjari_thin_path = 'font/Manjari-thin.ttf'
+
+dark = "#3B3365"
+medium = "#8F81DD"
+light = "#BCADFF"
+peach = "#FFEEDD"
+
+logo_img = mpimg.imread('Media/horizontal-logo.png')
+
+# Load the custom font
+manjari_bold = fm.FontProperties(fname=manjari_bold_path)
+manjari_regular = fm.FontProperties(fname=manjari_regular_path)
+manjari_thin = fm.FontProperties(fname=manjari_thin_path)
+
+# Get all .json files from the folder
+json_files = [f for f in glob.glob(os.path.join(folder_path, "*.json")) if 'manual_add.json' not in f]
+
+# Sort files by creation date
+json_files_sorted = sorted(json_files, key=os.path.getctime, reverse=True)
+i = 0
+entry = 0
+with open('json_log.csv', 'a+') as file:
+# Move the cursor to the beginning of the file
+    file.seek(0)
+    # Read the current content (if needed)
+    first_line = file.readline().strip()
+
+    if first_line:
+        file_name = first_line.split(',')[0]
+        entry_number = first_line.split(',')[1]
+        for json in json_files_sorted:
+            if os.path.basename(json)==file_name:
+                break
+            i += 1
+while i < len(json_files_sorted):
+    with open(json_files_sorted[i], 'r') as json_file:
+        data = json.load(json_file) 
+        eof = False
+        for e in range(entry,len(data)):
+            try:
+                exec(data[e]["output"])
+            except:
+                #fails to run - don't add!
+    i += 1
+    
+
+    
+
+    # If you need to write something, you can use file.write()
+    # Example: file.write("New log entry\n")
+    
+    # You can now read from or write to the file
+
--- new_data.py
+++ new_data.py
@@ -1,8 +1,7 @@
 import json
 import pandas as pd
 
-machine_over_time = """
-sales_data = df.groupby('Date')['total'].sum().reset_index()
+machine_over_time = """sales_data = df.groupby('Date')['total'].sum().reset_index()
 fig, ax = plt.subplots()
 ax.plot(sales_data['Date'], sales_data['total'], color=dark, linewidth=2)
 ax.set_xlabel('Date', color=dark, font_properties=manjari_bold)
@@ -16,11 +15,9 @@ for label in ax.get_xticklabels() + ax.get_yticklabels():
 logo_ax = fig.add_axes([0.8, 0.85, 0.15, 0.15], anchor='NE', zorder=1)
 logo_ax.imshow(logo_img)
 logo_ax.axis('off') 
-plt.show()
-"""
+plt.show()"""
 
-machine_over_time_july = """
-sales_data = df[(df['action_time']>='2024-07-01') & (df['action_time']<='2024-07-31')].groupby('Date')['total'].sum().reset_index()
+machine_over_time_july = """sales_data = df[(df['action_time']>='2024-07-01') & (df['action_time']<='2024-07-31')].groupby('Date')['total'].sum().reset_index()
 fig, ax = plt.subplots()
 ax.plot(sales_data['Date'], sales_data['total'], color=dark, linewidth=2)
 ax.set_xlabel('Date', color=dark, font_properties=manjari_bold)
@@ -34,11 +31,9 @@ for label in ax.get_xticklabels() + ax.get_yticklabels():
 logo_ax = fig.add_axes([0.8, 0.85, 0.15, 0.15], anchor='NE', zorder=1)
 logo_ax.imshow(logo_img)
 logo_ax.axis('off') 
-plt.show()
-"""
+plt.show()"""
 
-machine_over_time_august = """
-sales_data = df[(df['action_time']>='2024-08-01') & (df['action_time']<='2024-08-31')].groupby('Date')['total'].sum().reset_index()
+machine_over_time_august = """sales_data = df[(df['action_time']>='2024-08-01') & (df['action_time']<='2024-08-31')].groupby('Date')['total'].sum().reset_index()
 fig, ax = plt.subplots()
 ax.plot(sales_data['Date'], sales_data['total'], color=dark, linewidth=2)
 ax.set_xlabel('Date', color=dark, font_properties=manjari_bold)
@@ -52,11 +47,9 @@ for label in ax.get_xticklabels() + ax.get_yticklabels():
 logo_ax = fig.add_axes([0.8, 0.85, 0.15, 0.15], anchor='NE', zorder=1)
 logo_ax.imshow(logo_img)
 logo_ax.axis('off') 
-plt.show()
-"""
+plt.show()"""
 
-by_people = """
-staff_data = df.groupby('staff')['total'].sum().reset_index()
+by_people = """staff_data = df.groupby('staff')['total'].sum().reset_index()
 fig, ax = plt.subplots()
 ax.bar(staff_data['staff'], staff_data['total'], color=medium, linewidth=2)
 ax.set_xlabel('Staff', color=dark, font_properties=manjari_bold)
@@ -69,16 +62,45 @@ for label in ax.get_xticklabels() + ax.get_yticklabels():
 logo_ax = fig.add_axes([0.8, 0.85, 0.15, 0.15], anchor='NE', zorder=1)
 logo_ax.imshow(logo_img)
 logo_ax.axis('off') 
-plt.show()
-"""
+plt.show()"""
 
-table_albums_month = """
-albumn_data = df[(df['type']=='Physical') & (df['product'])]
+table_albums_month = """albumn_data = df[(df['type']=='Physical') & (df['product'])]
 albumn_data['action_time'] = pd.to_datetime(df['action_time'], errors='coerce')
 monthly_sales = albumn_data.groupby(pd.Grouper(key='action_time', freq='M'))['quantity'].sum().reset_index()
 monthly_sales.columns = ['Month','Total Albums']
-monthly_sales
-"""
+monthly_sales"""
+
+replacement_count = """staff_replacement_days = df.groupby('replaced')['Date'].nunique().reset_index(name='Days')
+staff_replacement_days = staff_replacement_days.dropna()
+all_staff = df['staff'].unique()
+all_staff_df = pd.DataFrame(all_staff, columns=['staff'])
+staff_replacement_days_complete = all_staff_df.merge(staff_replacement_days, left_on='staff', right_on='replaced', how='left')
+staff_replacement_days_complete['Days'] = staff_replacement_days_complete['Days'].fillna(0)
+staff_replacement_days_complete = staff_replacement_days_complete.drop(columns=['replaced'])
+staff_replacement_days_complete = staff_replacement_days_complete.sort_values(by='Days', ascending=False)
+
+fig, ax = plt.subplots()
+ax.bar(staff_replacement_days_complete['staff'], staff_replacement_days_complete['Days'], color=medium)
+ax.set_xlabel('Staff', color=dark, font_properties=manjari_bold)
+ax.set_ylabel('Days', color=dark, font_properties=manjari_bold)
+plt.xticks(rotation=45)
+ax.set_title('Count of Shifts that Needed Replacing', color=dark, font_properties=manjari_bold)
+ax.tick_params(colors=dark)
+for label in ax.get_xticklabels() + ax.get_yticklabels():
+    label.set_fontproperties(manjari_regular)
+logo_ax = fig.add_axes([0.8, 0.85, 0.15, 0.15], anchor='NE', zorder=1)
+logo_ax.imshow(logo_img)
+logo_ax.axis('off') 
+plt.show()"""
+
+replacement_count_table = """staff_replacement_days = df.groupby('replaced')['Date'].nunique().reset_index(name='Days')
+staff_replacement_days = staff_replacement_days.dropna()
+all_staff = df['staff'].unique()
+all_staff_df = pd.DataFrame(all_staff, columns=['staff'])
+staff_replacement_days_complete = all_staff_df.merge(staff_replacement_days, left_on='staff', right_on='replaced', how='left')
+staff_replacement_days_complete['Days'] = staff_replacement_days_complete['Days'].fillna(0)
+staff_replacement_days_complete = staff_replacement_days_complete.drop(columns=['replaced'])
+staff_replacement_days_complete.sort_values(by='Days',ascending=False)"""
 
 data = [
 {
@@ -100,11 +122,19 @@ data = [
 {
     "instruction": "Can I have machine sales over time for august?",
     "output": machine_over_time_august
+},
+{
+    "instruction": "Hi, can you show me in a table the number of times employees needed to be replaced on a shift",
+    "output": replacement_count_table
+},
+{
+    "instruction": "Show how many employees needed to be replaced on a shift",
+    "output": replacement_count
 }
 
 
 ]
 
 
-with open('ProcessedData/data.json', 'w') as json_file:
+with open('Training/manual_add.json', 'w') as json_file:
     json.dump(data, json_file, indent=4) 
